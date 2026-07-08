683030dbc2602a411b017ac8e4b77dc5f7b5a740
Darshan Singh <darshan.s@kalvium.community>
2026-07-08 13:48:34 +0530

    Update database.js

--- Server/src/config/database.js
+++ Server/src/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 
 const ConnectDB = async () => {
-    const uri = process.env.MONGODB_URI || "mongodb+srv://akshitpjt07:akiop1114@cluster0.zwuzzvp.mongodb.net/devTinder"
+    const uri = "mongodb+srv://akshitpjt07:akiop1114@cluster0.zwuzzvp.mongodb.net/devTinder"
     mongoose.connection.on('connected', () => {
         console.log('DB connected')
     })
